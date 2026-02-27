e5deecbe9a6df21a3055cbe16c724692705163fd
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-02-27 22:45:23 +0000

    created function that fetches data from the backend/ reads the data.json file

--- public/script.js
+++ public/script.js
@@ -2,9 +2,29 @@ let input = document.getElementById("input");
 let buttonEl = document.getElementById("add-button");
 let tasklist = document.getElementById("tasklist");
 
-let tasks = [] 
-// store tasks inside of an array
 
-async function loadAll() {
+async function getNotes() {
+    
+    const response = await fetch("/data");
+    const notes = await response.json();
+    tasklist.innerHTML = "";
+
+    notes.forEach(note => {
+        const li = document.createElement("li");
+        li.textContent = note.name;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+
+        deleteButton.addEventListener("click", async () => {
+            await fetch(`/data/${note.id}`, {method: "DELETE"});
+            getNotes();
+        });
+
+        li.appendChild(deleteButton);
+        tasklist.appendChild(li);
+    });
     
 }
+
+getNotes();
